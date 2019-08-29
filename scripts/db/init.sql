c2cde70e2444e1c8af0537577dc15dbe07271f16
Alexander Sundström <alexander.sundstrom@dynabyte.se>
2019-08-29 16:22:25 +0200

    Branch for Adam to use when swiching computer.

--- scripts/db/init.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE rest;
-
-\connect rest;
-
-CREATE USER springbootuser WITH ENCRYPTED PASSWORD 'admin';
-GRANT ALL PRIVILEGES ON DATABASE rest TO springbootuser;
--- src/main/java/rest/controller/MockedPersonController.java
+++ src/main/java/rest/controller/MockedPersonController.java
@@ -1,10 +1,12 @@
 package rest.controller;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import rest.model.Person;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/mocked/person")
@@ -17,33 +19,9 @@ public class MockedPersonController {
             new Person(4L, "Mia", "Pia")
     );
 
-
     @RequestMapping(method = RequestMethod.GET)
     public List<Person> getPersons() {
         return persons;
-    }
-
-    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
-    public Optional<Person> getPerson(@PathVariable Long id) {
-        return persons
-                .stream()
-                .filter(person -> person.getId().equals(id))
-                .findFirst();
-    }
-
-    @RequestMapping(method = RequestMethod.POST)
-    public Person createPerson(@RequestBody Person person) {
-        return new Person(2L, person.getFirstname(), person.getLastname());
-    }
-
-    @RequestMapping(method = RequestMethod.PUT, value = "/{id}")
-    public Person updatePerson(@PathVariable Long id, @RequestBody Person person) {
-        return new Person(id, person.getFirstname(), person.getLastname());
-    }
-
-    @RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
-    public void deletePerson(@PathVariable Long id) {
-        //        should call service to delete entity
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/ping")
